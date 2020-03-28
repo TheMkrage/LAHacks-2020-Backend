ba1cfc7f1df799712653c896652c2bcbde5a2cb4
Matthew Krager <matthewkrager@gmail.com>
2020-03-28 13:39:24 -0700

    iniital

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask
+from flask import request
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello, World!'
+
+@app.route('/upload', methods=['POST'])
+def handle_form():
+    data = request.data
+    print(data)
+    print(request.files)
