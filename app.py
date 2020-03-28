d2163e7663af0c107f3a927e73bfd00ceae059ee
Matthew Krager <matthewkrager@gmail.com>
2020-03-28 16:37:36 -0700

    new try

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask
 from flask import request
 import base64
+import subprocess
 
 from os import environ, path, system
 
 from pocketsphinx.pocketsphinx import *
 from sphinxbase.sphinxbase import *
+import pronouncing
 
 app = Flask(__name__)
 
@@ -15,20 +17,63 @@ def hello_world():
 
 @app.route('/upload', methods=['POST'])
 def handle_form():
-    data = request.data
+    word = request.json['word']
+    expected = pronouncing.phones_for_word(word)
 
+    data = request.data
     decoded = base64.b64decode(request.json['base'])
-    media_write = open('test.m4a', 'w')
-    media_write.write(decoded)
+    with open('test.m4a', 'w') as media_write:
+        media_write.write(decoded)
 
     phonemes = phonemes_for_audio()
-    print(phonemes)
+    score = score_for_pronunciation(expected, phonemes)
     return 'response'
 
 
-def phonemes_for_audio():
-    system("ffmpeg -i test.m4a -ar 16000 -ac 1 using.wav")
+def score_for_pronunciation(expected, actual):
+    def filtering(seg):
+        letters = ['SIL', '+SPN+']
+        if (seg not in letters):
+            return True
+        else:
+            return False
+    expected_string = " ".join(expected).replace("1", "").replace("0", "").replace("2", "")
+    actual = filter(filtering, actual)
+    actual_string = " ".join(actual)
 
+    expected_vowels, expected_cons = get_vowels_and_cons(expected[0].split(" "))
+    actual_vowels, actual_cons = get_vowels_and_cons(actual)
+
+    v_c_score = abs(expected_vowels-actual_vowels) + abs(expected_cons-actual_cons)
+
+
+    print(expected_string)
+    print(actual_string)
+
+    print(v_c_score)
+    print(levenshtein_distance(expected_string, actual_string))
+
+
+def get_vowels_and_cons(phonemes):
+    expected_vowels = 0
+    expected_cons = 0
+    is_last_vowel = False
+    print(phonemes)
+    for phoneme in phonemes:
+        print(phoneme)
+        print(phoneme[0])
+        if phoneme[0] in ['A', 'E', 'I', 'O', 'U']:
+            expected_vowels += 1
+            is_last_vowel = True
+        else:
+            if (is_last_vowel):
+                expected_cons += 1
+            is_last_vowel = False
+    return expected_vowels, expected_cons
+
+def phonemes_for_audio():
+    # system()
+    subprocess.call(["ffmpeg", "-y", "-i", "test.m4a", "-ar", "16000", "-ac", "1", "using.wav"])
     MODELDIR = "model"
     DATADIR = ""
 
@@ -55,3 +100,28 @@ def phonemes_for_audio():
 
     hypothesis = decoder.hyp()
     print ('Phonemes: ', [seg.word for seg in decoder.seg()])
+    return [seg.word for seg in decoder.seg()]
+
+def levenshtein_distance(string1, string2):
+    n = len(string1)
+    m = len(string2)
+    d = [[0 for x in range(n + 1)] for y in range(m + 1)]
+
+    for i in range(1, m + 1):
+        d[i][0] = i
+
+    for j in range(1, n + 1):
+        d[0][j] = j
+
+    for j in range(1, n + 1):
+        for i in range(1, m + 1):
+            if string1[j - 1] is string2[i - 1]:
+                delta = 0
+            else:
+                delta = 1
+
+            d[i][j] = min(d[i - 1][j] + 1,
+                          d[i][j - 1] + 1,
+                          d[i - 1][j - 1] + delta)
+
+    return d[m][n]
